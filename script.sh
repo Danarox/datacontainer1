f38657a9f0af317f6a439781fbefd398ee9b981a
root <root@localhost.asix>
2018-02-07 21:36:06 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+docker network create ejercicio5vol
+
+docker create --name datacontainer_danarox -v /data:/var/www/html -v /data:/usr/share/nginx/html busybox
+
+/*basada en debian--habra que usar apt-get update para instalar el nano*/
+docker run -itd -p 8011:80 --network ejercicio5vol --volumes-from datacontainer_danarox --name nginx nginx
+
+docker run -itd -p 8030:80 --network ejercicio5vol --volumes-from datacontainer_danarox --name apache danarox/centos-httpd-php
